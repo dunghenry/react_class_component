64ac8eda4c0b66bd4761cf378e7321fbc8431190
dunghenry <trandungksnb6930082001@gmail.com>
2022-08-20 18:23:06 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Welcome React ClassComponent</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
